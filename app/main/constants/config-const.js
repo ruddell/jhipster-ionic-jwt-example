4e45315cb481ca2bc8f7ad5c400c5420ca6626db
Jonathan Ruddell <jonathan.ruddell@gmail.com>
2016-05-13 09:40:44 -0600

    Init with m-ionic code

---
+++ app/main/constants/config-const.js
@@ -0,0 +1,17 @@
+'use strict';
+angular.module('main')
+.constant('Config', {
+
+  // gulp environment: injects environment vars
+  ENV: {
+    /*inject-env*/
+    /*endinject*/
+  },
+
+  // gulp build-vars: injects build vars
+  BUILD: {
+    /*inject-build*/
+    /*endinject*/
+  }
+
+});
